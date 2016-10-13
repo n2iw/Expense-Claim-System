d82d797e0ff033465acf272a3b6b5a0bead7b9dd
Ming Ying <yingming18@gmail.com>
2016-10-12 22:44:58 -0400

    Move to Spring Boot and Maven setup

---
+++ src/main/java/edu/ualbany/icis518/team6/ServletInitializer.java
@@ -0,0 +1,13 @@
+package edu.ualbany.icis518.team6;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EcsApplication.class);
+	}
+
+}
---
+++ src/test/java/edu/ualbany/icis518/team6/EcsApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.ualbany.icis518.team6;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EcsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
