7bac27fc45629bee7002d4a2967303f4142d1a5d
lovelymis <gladysharma282@gmail.com>
2016-10-22 00:23:00 -0400

    Create Model Class (Remaining)
    Trip, EmployeeTrip,Expense,ExpenseType.

--- src/test/java/edu/ualbany/icis518/team6/EmployeeTripData.java
+++ src/test/java/edu/ualbany/icis518/team6/EmployeeTripData.java
@@ -4,25 +4,24 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class ProjectsData {
+public class EmployeeTripData {
 
 	public static void main(String[] args) {
-		//Instantiate details of Projects class
-		Projects prj = new Projects(); 
-		prj.setProjectId(51);
-		prj.setBudget(8000);
-		prj.setProjectName("ATM");
-		prj.setPmId(1001);
-			
+		
+		//Instantiate details of EmployeeTrip class
+		EmployeeTrip emp_trp = new EmployeeTrip(); 
+		emp_trp.setProjectId(51);
+		emp_trp.setEmployeeId(001);
+							
 		//Use hibernate API
 		SessionFactory factory = new Configuration().configure().buildSessionFactory();
 		Session session = factory.openSession();
-		
+				
 		//Manage transaction
 		session.beginTransaction();
-		session.save(prj);
+		session.save(emp_trp);
 		session.getTransaction().commit();
 		assert(true);
 	}
-		
+
 }
--- src/test/java/edu/ualbany/icis518/team6/TripsData.java
+++ src/test/java/edu/ualbany/icis518/team6/TripsData.java
@@ -4,25 +4,28 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class ProjectsData {
+public class TripsData {
 
 	public static void main(String[] args) {
-		//Instantiate details of Projects class
-		Projects prj = new Projects(); 
-		prj.setProjectId(51);
-		prj.setBudget(8000);
-		prj.setProjectName("ATM");
-		prj.setPmId(1001);
-			
+		
+		//Instantiate details of Trips class
+		Trips trp = new Trips();
+		trp.setTripId(701);
+		trp.setProjectId(51);
+		trp.setDescription("This is a trip to New York for ATM project");
+	    trp.setStartDate("10-02-2016");
+	    trp.setEndDate("10-01-2017");
+					
 		//Use hibernate API
 		SessionFactory factory = new Configuration().configure().buildSessionFactory();
 		Session session = factory.openSession();
-		
+				
 		//Manage transaction
 		session.beginTransaction();
-		session.save(prj);
+		session.save(trp);
 		session.getTransaction().commit();
 		assert(true);
+			
 	}
-		
+
 }
