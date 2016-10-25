29443caa2e30f2da57becef8664c7dd9069a57d5
Ming Ying <yingming18@gmail.com>
2016-10-25 01:57:01 -0400

    Finish Employee homepage

--- src/main/java/edu/ualbany/icis518/team6/controller/EmployeeController.java
+++ src/main/java/edu/ualbany/icis518/team6/controller/EmployeeController.java
@@ -1,18 +1,36 @@
 package edu.ualbany.icis518.team6.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import edu.ualbany.icis518.team6.Employee;
+import edu.ualbany.icis518.team6.Expense;
+import edu.ualbany.icis518.team6.Trips;
 
 @Controller
 public class EmployeeController {
 	
 	@RequestMapping("/employee")
-	public String employeeHomePage() {
+	public String employeeHomePage(@RequestParam(value="id", required=true) int id,
+			Model model) {
+		Employee e = Employee.getbyEmployeeId(id);
+		List<Trips> trips = e.getAllMyTrips();
+		model.addAttribute("trips", trips);
+		model.addAttribute("id", id);
 		return "employee";
 	}
 
 	@RequestMapping("/employee/trip")
-	public String showTrip() {
+	public String showTrip(@RequestParam(value="id", required=true) int id,
+			@RequestParam(value="tripId", required=true) int tripId,
+			Model model) {
+		Employee e = Employee.getbyEmployeeId(id);
+		List<Expense> exps =  e.getAllMyExpense();
+		model.addAttribute("expenses", exps);
 		return "employee_form";
 	}
 	
