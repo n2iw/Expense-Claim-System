00c26105943ab3e67e34c36b11d1cbd488ef1e67
Ming Ying <yingming18@gmail.com>
2016-10-25 11:23:39 -0400

    Finish Manager controller

--- src/main/java/edu/ualbany/icis518/team6/controller/ManagerController.java
+++ src/main/java/edu/ualbany/icis518/team6/controller/ManagerController.java
@@ -1,18 +1,32 @@
 package edu.ualbany.icis518.team6.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import edu.ualbany.icis518.team6.Employee;
+import edu.ualbany.icis518.team6.Projects;
+
+import java.util.List;
 
 @Controller
 public class ManagerController {
 	
 	@RequestMapping("/manager")
-	public String managerHomePage() {
+	public String managerHomePage(@RequestParam(value="id", defaultValue="1") int id,
+			Model model) {
+		Employee e = Employee.getbyEmployeeId(id);
+		List<Projects> projects = Projects.getbyProjectManager(e);
+		model.addAttribute("projects", projects);
 		return "manager";
 	}
 
 	@RequestMapping("/manager/project")
-	public String showProject() {
+	public String showProject(@RequestParam(value="id", required=true) int id,
+			Model model) {
+		Projects project = Projects.getbyProjectId(id);
+		model.addAttribute("project", project);
 		return "project_details";
 	}
 
@@ -24,7 +38,7 @@ public class ManagerController {
 	@RequestMapping("/manager/project/create")
 	public String createProject() {
 		//create project and redirect user
-		return "redirect:/manager/project";
+		return "redirect:/manager/project?id=1";
 	}
 	
 	@RequestMapping("/manager/trip/new")
