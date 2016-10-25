0fab663615e22ac85f1bec42a535427d85630694
Ming Ying <yingming18@gmail.com>
2016-10-24 21:50:52 -0400

    Add Manger Controller and edit views accordingly

---
+++ src/main/java/edu/ualbany/icis518/team6/controller/ManagerController.java
@@ -0,0 +1,40 @@
+package edu.ualbany.icis518.team6.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ManagerController {
+	
+	@RequestMapping("/manager")
+	public String managerHomePage() {
+		return "manager";
+	}
+
+	@RequestMapping("/manager/project")
+	public String showProject() {
+		return "project_details";
+	}
+
+	@RequestMapping("/manager/project/new")
+	public String newProject() {
+		return "addProject";
+	}
+	
+	@RequestMapping("/manager/project/create")
+	public String createProject() {
+		//create project and redirect user
+		return "project_details";
+	}
+	
+	@RequestMapping("/manager/trip/new")
+	public String newTrip() {
+		return "add_trip";
+	}
+	
+	@RequestMapping("/manager/trip/create")
+	public String createTrip() {
+		//create project and redirect user
+		return "/manager";
+	}
+}
