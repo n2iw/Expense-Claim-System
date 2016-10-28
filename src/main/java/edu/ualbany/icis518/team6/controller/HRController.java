38004fef4829f7cd372099696d8985c62df7e164
Ming Ying <yingming18@gmail.com>
2016-10-27 20:34:30 -0400

    Rename HrController.java to HRController.java

--- src/main/java/edu/ualbany/icis518/team6/controller/HRController.java
+++ src/main/java/edu/ualbany/icis518/team6/controller/HRController.java
@@ -1,18 +1,29 @@
 package edu.ualbany.icis518.team6.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import edu.ualbany.icis518.team6.Expense;
 
 @Controller
-public class HRController {
+public class HrController {
 	
 	@RequestMapping("/hr")
-	public String hrHomePage() {
+	public String hrHomePage(Model model) {
+		Expense exp = new Expense();
+		List<Expense> expList = exp.getAllExpense();
+		model.addAttribute("expList", expList);
 		return "hr";
 	}
 
 	@RequestMapping("/hr/claim")
-	public String showClaim() {
+	public String showClaim(@RequestParam(value="id", required=true) int id, Model model) {
+		Expense exp = new Expense().getbyExpenseId(id);
+		model.addAttribute("expense", exp);
 		return "show_claim";
 	}
 }
