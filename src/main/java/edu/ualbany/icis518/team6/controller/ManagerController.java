135a2ddade79a4585e97db0bac22179d28defb79
Ming Ying <yingming18@gmail.com>
2016-11-14 17:08:34 -0500

    Create new project

--- src/main/java/edu/ualbany/icis518/team6/controller/ManagerController.java
+++ src/main/java/edu/ualbany/icis518/team6/controller/ManagerController.java
@@ -2,8 +2,12 @@ package edu.ualbany.icis518.team6.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -32,7 +36,7 @@ public class ManagerController {
 		if (employee == null) {
 			return "redirect:/";
 		}
-		if (!employee.getRole().equals("manager")) {
+		if (!employee.getRole().equalsIgnoreCase("manager")) {
 			return "redirect:/employee";
 		}
 		List<Projects> projects = Projects.getbyProjectManager(employee);
@@ -41,81 +45,75 @@ public class ManagerController {
 	}
 
 	@GetMapping("/project/{projectId}")
-	public String showProject(@PathVariable int id,
-			@PathVariable int projectId,
-			HttpSession session, 
-			Model model) {
+	public String showProject( @PathVariable int projectId, HttpSession session, Model model) {
 		Employee employee = getEmployee(session);
 		if (employee == null) {
 			return "redirect:/";
 		}
-		if (!employee.getRole().equals("manager")) {
+		if (!employee.getRole().equalsIgnoreCase("manager")) {
 			return "redirect:/employee";
 		}
 		Projects project = Projects.getbyProjectId(projectId);
+		if (project == null) {
+			System.out.println("ManagerController: project not found for id=" + projectId);
+			return "/project";
+		}
 		model.addAttribute("project", project);
 		return "project_details";
 	}
 
 	@GetMapping("/project/new")
-	public String newProject(@PathVariable int id,
-			@PathVariable int projectId,
-			HttpSession session,
-			Model model) {
+	public String newProject(HttpSession session, Model model) {
 		Employee employee = getEmployee(session);
 		if (employee == null) {
 			return "redirect:/";
 		}
-		if (!employee.getRole().equals("manager")) {
+		if (!employee.getRole().equalsIgnoreCase("manager")) {
 			return "redirect:/employee";
 		}
 		return "addProject";
 	}
 	
-	@GetMapping("project/create")
-	public String createProject(@PathVariable int id,
-			@PathVariable int projectId,
-			HttpSession session,
-			Model model) {
+	@PostMapping("/project")
+	public String createProject(@RequestBody MultiValueMap<String,String> formData, HttpSession session, Model model) {
 		Employee employee = getEmployee(session);
 		if (employee == null) {
 			return "redirect:/";
 		}
-		if (!employee.getRole().equals("manager")) {
+		if (!employee.getRole().equalsIgnoreCase("manager")) {
 			return "redirect:/employee";
 		}
-		//create project and redirect user
-		return "redirect:/manager/" + id + "/project";
+//		System.out.println(formData);
+		Projects newProject = new Projects();
+		newProject.setProjectManager(employee);
+		newProject.setBudget(Integer.parseInt(formData.getFirst("budget")));
+		newProject.setProjectName(formData.getFirst("name"));
+		newProject.save();
+		return "redirect:/manager";
 	}
 	
 	@GetMapping("/trip/new")
-	public String newTrip(@PathVariable int id,
-			@PathVariable int projectId,
-			HttpSession session,
-			Model model) {
+	public String newTrip(HttpSession session, Model model) {
 		Employee employee = getEmployee(session);
 		if (employee == null) {
 			return "redirect:/";
 		}
-		if (!employee.getRole().equals("manager")) {
+		if (!employee.getRole().equalsIgnoreCase("manager")) {
 			return "redirect:/employee";
 		}
 		return "add_trip";
 	}
 	
-	@GetMapping("/trip/create")
-	public String createTrip(@PathVariable int id,
-			@PathVariable int projectId,
-			HttpSession session,
-			Model model) {
+	@PostMapping("/trip")
+	public String createTrip(HttpSession session, Model model) {
 		Employee employee = getEmployee(session);
 		if (employee == null) {
 			return "redirect:/";
 		}
-		if (!employee.getRole().equals("manager")) {
+		if (!employee.getRole().equalsIgnoreCase("manager")) {
 			return "redirect:/employee";
 		}
 		//create project and redirect user
-		return "redirect:/manager/" + id;
+		return "redirect:/manager";
 	}
 }
