5645b480b86dbfbbb714a1982e1fc6837cd10e59
Ming Ying <yingming18@gmail.com>
2016-11-14 12:39:39 -0500

    Add more code for controllers

--- src/main/java/edu/ualbany/icis518/team6/controller/ManagerController.java
+++ src/main/java/edu/ualbany/icis518/team6/controller/ManagerController.java
@@ -2,6 +2,7 @@ package edu.ualbany.icis518.team6.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -10,49 +11,80 @@ import edu.ualbany.icis518.team6.Projects;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
+@RequestMapping("/manager/{id}")
 public class ManagerController {
 	
-	@RequestMapping("/manager")
-	public String managerHomePage(@RequestParam(value="id", defaultValue="-1") int id,
-			Model model) {
-		if (id == -1) {
-			return "redirect:/manager?id=1";
+	private Employee getEmployee(HttpSession session, int id) {
+		Employee e;
+		Object obj = session.getAttribute("employee");
+		if (obj != null && obj instanceof Employee) {
+			e = (Employee)obj;
+		} else {
+			e = Employee.getbyEmployeeId(id);
+			session.setAttribute("employee", e);
 		}
-		Employee e = Employee.getbyEmployeeId(id);
+		return e;
+	}
+	
+	@RequestMapping("")
+	public String managerHomePage(@PathVariable int id,
+			HttpSession session, Model model) {
+		Employee e = getEmployee(session, id);
 		//TODO: Need to check if this id is a manager or not
 		List<Projects> projects = Projects.getbyProjectManager(e);
 		model.addAttribute("projects", projects);
 		return "manager";
 	}
 
-	@RequestMapping("/manager/project")
-	public String showProject(@RequestParam(value="id", required=true) int id,
+	@RequestMapping("/project/{projectId}")
+	public String showProject(@PathVariable int id,
+			@PathVariable int projectId,
+			HttpSession session, 
 			Model model) {
-		Projects project = Projects.getbyProjectId(id);
+		Employee e = getEmployee(session, id);
+		Projects project = Projects.getbyProjectId(projectId);
 		model.addAttribute("project", project);
 		return "project_details";
 	}
 
-	@RequestMapping("/manager/project/new")
-	public String newProject() {
+	@RequestMapping("/project/new")
+	public String newProject(@PathVariable int id,
+			@PathVariable int projectId,
+			HttpSession session,
+			Model model) {
+		Employee e = getEmployee(session, id);
 		return "addProject";
 	}
 	
-	@RequestMapping("/manager/project/create")
-	public String createProject() {
+	@RequestMapping("project/create")
+	public String createProject(@PathVariable int id,
+			@PathVariable int projectId,
+			HttpSession session,
+			Model model) {
+		Employee e = getEmployee(session, id);
 		//create project and redirect user
-		return "redirect:/manager/project?id=1";
+		return "redirect:/manager/" + id + "/project";
 	}
 	
-	@RequestMapping("/manager/trip/new")
-	public String newTrip() {
+	@RequestMapping("/trip/new")
+	public String newTrip(@PathVariable int id,
+			@PathVariable int projectId,
+			HttpSession session,
+			Model model) {
+		Employee e = getEmployee(session, id);
 		return "add_trip";
 	}
 	
-	@RequestMapping("/manager/trip/create")
-	public String createTrip() {
+	@RequestMapping("/trip/create")
+	public String createTrip(@PathVariable int id,
+			@PathVariable int projectId,
+			HttpSession session,
+			Model model) {
+		Employee e = getEmployee(session, id);
 		//create project and redirect user
-		return "redirect:/manager";
+		return "redirect:/manager/" + id;
 	}
 }
