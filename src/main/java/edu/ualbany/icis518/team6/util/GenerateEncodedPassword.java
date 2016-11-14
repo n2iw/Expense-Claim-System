308debe8c742ff648f2bceb5ecd4d63cb9119fdf
SayaliThorat24 <sayali.thorat24@gmail.com>
2016-11-14 12:41:07 -0500

    Added code to encode password

--- src/main/java/edu/ualbany/icis518/team6/util/GenerateEncodedPassword.java
+++ src/main/java/edu/ualbany/icis518/team6/util/GenerateEncodedPassword.java
@@ -1,6 +1,9 @@
 package edu.ualbany.icis518.team6.util;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import java.util.Base64;
+
+import org.springframework.context.annotation.Bean;
+
 
 public class GenerateEncodedPassword {
 	
@@ -8,10 +11,9 @@ public class GenerateEncodedPassword {
 	 * This method gives encoded password.
 	 * We are using Spring BCryptPasswordEncoder to encrypt password.
 	 */
+	@Bean
 	public String getEncodedPassword(String password) {
-		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-		String hashedPassword = passwordEncoder.encode(password);
-		System.out.println(hashedPassword);
-		return hashedPassword;
+		String base64encodedString = Base64.getEncoder().encodeToString(password.getBytes());
+		return base64encodedString;
 	  }
 }
